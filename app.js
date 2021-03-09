d9506254bff50160c0d6b086c7b517bd6aca74a7
Juned Khan <junaidkhanhere@gmail.com>
2021-03-09 21:13:18 +0530

    security enhancement

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const morgan = require('morgan');
 const express = require('express');
-const AppError = require('./utils/appError');
 
+const AppError = require('./utils/appError');
 // Controllers
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,17 +15,50 @@ const globalErrorHandler = require('./controllers/errorController');
 // Express app
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+
+// Set security headers using helmet
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Middleware to parse req body to json
-app.use(express.json());
+// middleware to limit requests from one IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, try again in an hour'
+});
+app.use('/api', limiter);
 
-// TO send html pages as response
+// body-parser: reading from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+// Data sanitization againstcross site scripting attacks
+app.use(xss());
+
+// Prevents paramter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //   console.log(req.headers);
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,9 +14,26 @@ const signinToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signinToken(user.id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  //   SET COOKIE SECURE OPTION TO TRUE IN PRODUCTION
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  //   remove password from response
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
-    token
+    token,
+    data: {
+      user
+    }
   });
 };
 
